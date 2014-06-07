d2f509cd1c99d68001447993aec68b406657f4e5
Andres Ospina <andresospina@MacBook-Pro-de-Andres.local>
2014-06-06 19:55:28 -0500

    Testing de campaña #GOMC
    Primer acercamiento de campaña pre show

--- enviar.php
+++ enviar.php
@@ -48,6 +48,39 @@
 	setTimeout ("redireccionar()", 1000);
 
 </script>
+
+
+<!-- Analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-51547003-1', 'manuelmedrano.com');
+	  ga('require', 'displayfeatures');
+	  ga('send', 'pageview');
+
+	  setTimeout('_gaq.push([\'_trackEvent\', \'NoBounce\', \'Over 30 seconds\'])',30000);
+
+	</script>
+
+<!-- Remarketing -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1016618282;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="http://googleads.g.doubleclick.net/pagead/viewthroughconversion/1016618282/?value=0&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
 </body>
 
 </html>
